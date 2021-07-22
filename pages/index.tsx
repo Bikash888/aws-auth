784b2b1d32bf87ea678bd8cb8c7f1dae98b94206
bikash.dulal <bikash.dulal@admin01011s-MacBook-Air.local>
2021-07-22 15:27:42 +0545

    confirmation code sent

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,74 @@
 
 import { Auth } from 'aws-amplify';
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, notification } from 'antd';
 import { SubmitHandler, Controller, useForm } from 'react-hook-form'
 
 interface LoginProps{
-  email?: string;
+  username?: string;
   password?: string;
 }
 
 export default function Home() {
-  const { control, handleSubmit } = useForm<LoginProps>();
+  const { control, handleSubmit,formState: { errors } } = useForm<LoginProps>();
   
 
-  const handleSignup = async() => {
+  const handleSignup = async (data) => {
+     console.log(data)
     try {
-      const { user } = await Auth.signUp({
-        username:'bikashdulal150@gmail.com',
-        password:"Asd@123456",
-       
-    });
-    console.log(user);
+       await Auth.signUp({
+        username: data?.username,
+        password: data?.password,
+        attributes: {
+          email: data?.email,
+        },
+        
+      })
+      
     } catch (err) {
-      console.log("---->",err)
+      notification.error({
+        message: "Error Occured",
+        description:err?.message,
+      })
+      
     
     }
   }
+  console.log("error",errors)
   return (
     <>
-      <Form layout='vertical' style={{maxWidth:'30%',margin:"0 auto",marginTop:"40px"}}>
+      <Form onFinish={handleSubmit(handleSignup)} layout='vertical' style={{maxWidth:'30%',margin:"0 auto",marginTop:"40px"}}>
         <Form.Item label="Email Address">
           <Controller
-            name="email"
+            name="username"
+            rules={{required:true}}
              control={control}
             render={({ field }) => <Input size="large" {...field}/>}
           />
+          {
+       errors.username && <span>Email is required</span>
+        }
         
         </Form.Item>
-        <Form.Item label="Email Address">
+        
+        <Form.Item label="Password">
           <Controller
             name="password"
+            rules={{
+              required: true,
+              minLength:8,
+              
+            }}
              control={control}
             render={({ field }) => <Input.Password size="large" {...field}/>}
           />
+           {
+       errors.password && <span>{errors?.password?.type==="minLength"?"Minimum length is 8":"password is required"}</span>
+        }
         
         </Form.Item>
         <Form.Item>
 
-          <Button type='ghost'>Register</Button>
+          <Button htmlType="submit" size="large" type='ghost'>Register</Button>
         </Form.Item>
        
         
